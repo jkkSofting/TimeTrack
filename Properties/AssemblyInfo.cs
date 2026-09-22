00ce51d0f1d54f12d36cd5b3eb1395cd59317bb2
Janick Kaltenmark <janick.kaltenmark@softing.com>
2026-09-22 14:12:50 +0200

    Bump version to 2026.39.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2026.39.4")]
-[assembly: AssemblyFileVersion("2026.39.4")]
+[assembly: AssemblyVersion("2026.39.5")]
+[assembly: AssemblyFileVersion("2026.39.5")]
